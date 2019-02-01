b6716196d72f8f9b8ce5830c461333572408b97b
Alan Ridlehoover and Fito von Zastrow <dev+alan+fito@entelo.com>
2019-01-31 18:58:34 -0800

    Adds logging to create alert job

--- app/jobs/create_alert_job.rb
+++ app/jobs/create_alert_job.rb
@@ -2,6 +2,13 @@ class CreateAlertJob < ApplicationJob
   ALLOWED_ATTRIBUTES = ['uuid', 'title', 'location', 'message', 'publish_at', 'effective_at', 'expires_at'].freeze
 
   def perform(alert_attributes)
-    Alert.create(alert_attributes.slice(*ALLOWED_ATTRIBUTES))
+    allowed_alert_params = alert_attributes.slice(*ALLOWED_ATTRIBUTES)
+    alert = Alert.create(allowed_alert_params)
+
+    if alert.persisted?
+      WASLogger.json(action: :create_alert, status: :succeeded, params: allowed_alert_params)
+    else
+      WASLogger.json(action: :create_alert, status: :failed, params: allowed_alert_params)
+    end
   end
 end
--- spec/jobs/create_alert_job_spec.rb
+++ spec/jobs/create_alert_job_spec.rb
@@ -25,15 +25,34 @@ describe CreateAlertJob, type: :job do
     let(:publish_at) { 'publish_at' }
     let(:effective_at) { 'effective_at' }
     let(:expires_at) { 'expires_at' }
+    let(:alert) { instance_double(Alert, persisted?: persisted?) }
+    let(:persisted?) { true }
 
     before do
-      allow(Alert).to receive(:create)
+      allow(WASLogger).to receive(:json)
+      allow(Alert).to receive(:create).and_return(alert)
 
       perform
     end
 
     it 'creates an alert with the provided alert attributes' do
       expect(Alert).to have_received(:create).with(alert_attributes)
+    end
+
+    context 'when the alert record is successfully created' do
+      let(:persisted?) { true }
+
+      it 'logs success' do
+        expect(WASLogger).to have_received(:json).with(action: :create_alert, status: :succeeded, params: alert_attributes)
+      end
+    end
+
+    context 'when the alert record is NOT created' do
+      let(:persisted?) { false }
+
+      it 'logs failure' do
+        expect(WASLogger).to have_received(:json).with(action: :create_alert, status: :failed, params: alert_attributes)
+      end
     end
   end
 end
