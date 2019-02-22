f603b64c9fed8cc49bafce7dbfbca1492f837383
Alan Ridlehoover and Fito von Zastrow <dev+alan+fito@entelo.com>
2019-02-22 05:27:12 -0800

    Cleans up SQSWorker to make duplication more visible

--- app/workers/sqs_worker.rb
+++ app/workers/sqs_worker.rb
@@ -4,34 +4,51 @@ class SQSWorker
   shoryuken_options queue: ENV['INPUT_QUEUE_NAME'], body_parser: :json
 
   def perform(sqs_message, body)
+    @sqs_message = sqs_message
+    @body = body
+
     case body['type']
     when 'create_alert'
-      alert = Alert.create(body.slice(*Alert::ALLOWED_ATTRIBUTES))
-      if alert.persisted?
-        sqs_message.delete
-        WASLogger.json(action: :create_alert, actor: :telemetry, status: :succeeded, params: body)
-      else
-        WASLogger.json(action: :create_alert, actor: :telemetry, status: :failed, params: body, errors: alert.errors.messages)
-      end
+      create_alert
     when 'create_recipient'
-      recipient = Recipient.create(body.slice(*Recipient::ALLOWED_ATTRIBUTES))
-      if recipient.persisted?
-        sqs_message.delete
-        WASLogger.json(action: :create_recipient, actor: :telecom, status: :succeeded, params: body)
-      else
-        WASLogger.json(action: :create_recipient, actor: :telecom, status: :failed, params: body, errors: recipient.errors.messages)
-      end
+      create_recipient
     when 'unsubscribe_recipient'
-      recipient = Recipient.find_by(body.slice(*Recipient::ALLOWED_ATTRIBUTES))
-
-      if recipient.present?
-        recipient.destroy
-        WASLogger.json(action: :unsubscribe_recipient, actor: :telecom, status: :succeeded, params: body)
-      else
-        WASLogger.json(action: :unsubscribe_recipient, actor: :telecom, status: :failed, params: body)
-      end
-
-      sqs_message.delete
+      unsubscribe_recipient
     end
+  end
+
+  private
+
+  def create_alert
+    alert = Alert.create(@body.slice(*Alert::ALLOWED_ATTRIBUTES))
+    if alert.persisted?
+      WASLogger.json(action: :create_alert, actor: :telemetry, status: :succeeded, params: @body)
+      @sqs_message.delete
+    else
+      WASLogger.json(action: :create_alert, actor: :telemetry, status: :failed, params: @body, errors: alert.errors.messages)
+    end
+  end
+
+  def create_recipient
+    recipient = Recipient.create(@body.slice(*Recipient::ALLOWED_ATTRIBUTES))
+    if recipient.persisted?
+      WASLogger.json(action: :create_recipient, actor: :telecom, status: :succeeded, params: @body)
+      @sqs_message.delete
+    else
+      WASLogger.json(action: :create_recipient, actor: :telecom, status: :failed, params: @body, errors: recipient.errors.messages)
+    end
+  end
+
+  def unsubscribe_recipient
+    recipient = Recipient.find_by(@body)
+
+    if recipient.present?
+      recipient.destroy
+      WASLogger.json(action: :unsubscribe_recipient, actor: :telecom, status: :succeeded, params: @body)
+    else
+      WASLogger.json(action: :unsubscribe_recipient, actor: :telecom, status: :failed, params: @body)
+    end
+
+    @sqs_message.delete
   end
 end
